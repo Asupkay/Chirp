140b1ecf47ea76261a53f966991ce825210cf473
Alex Supkay <asupkay1124@gmail.com>
2019-02-04 17:49:28 -0500

    Filter english tweets only

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,33 @@
 const express = require('express');
-const app = express();
 const configRoutes = require('./routes');
+const Twitter = require('twitter');
+require('dotenv').config();
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+let num = 0;
+client.stream('statuses/filter', {track: 'Google'}, (stream) => {
+  stream.on('data', (event) => {
+    const english = /^[A-Za-z0-9!?.@#$%^&*()\[\]"',+\-:i\/\s…’]*$/;
+
+    if(english.test(event.text)) {
+      console.log(event.text);
+    }
+    console.log(num);
+    num++;
+  });
+  
+  stream.on('error', (error) => {
+    throw error;
+  });
+});
+
+const app = express();
 
 configRoutes(app);
 
