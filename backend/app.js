729a4d6212827721f8ae14abd304fbefb184ce6b
Kyle <kyle.rozanitis@gmail.com>
2019-02-11 20:54:35 -0500

    configured Firebase & added functionality to save to realtime database

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,24 @@ const sentiment = new Sentiment();
 const Twitter = require('twitter');
 require('dotenv').config();
 
+var firebase = require('firebase');
+
+var config = {
+  apiKey: process.env.DB_API_KEY,
+  authDomain: process.env.DB_AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  storageBucket: process.env.STORAGE_BUCKET
+};
+
+firebase.initializeApp(config);
+
+function writeUserData(sentimentScore, timeStamp) {
+  firebase.database().ref('google/').push({
+    sentiment_score: sentimentScore,
+    time_stamp: timeStamp
+  });
+}
+
 const client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
@@ -16,15 +34,19 @@ const client = new Twitter({
 client.stream('statuses/filter', {track: 'Google'}, (stream) => {
   let sentimentScore = 0;
   let numTweets = 0;
+  let date = 0;
 
   setInterval(() => {
     console.log(`Average sentiment = ${sentimentScore/numTweets}`);
+    writeUserData(sentimentScore/numTweets, timeStamp);
     sentimentScore = 0;
     numTweets = 0;
   }, 60 * 1000)
 
   stream.on('data', (event) => {
     let lang = franc(event.text);
+    date = new Date();
+    timeStamp = date.toLocaleTimeString();
 
     if(lang == 'eng') {
       let tweetSentimentScore = sentiment.analyze(event.text);
