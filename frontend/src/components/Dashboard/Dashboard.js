7d7523a46ee7e0fc0ceebc1d34924d8f08186f62
Kyle <kyle.rozanitis@gmail.com>
2019-03-28 17:31:27 -0400

    Begin converting to semantic ui react

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,20 +1,17 @@
-import React, { Component } from "react";
-import firebase from "firebase";
-import Chart from "../chart/Chart_App";
-import Pie from "../Pie";
-import google from "./Google_logo.png";
-import Client from "../NewsFeed/Client";
-import "./Dashboard.css"
-import logo from "./Twitter.svg";
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import Chart from '../chart/Chart_App';
+import Pie from '../Pie';
+import Google from './Google_logo.png';
+import Client from '../NewsFeed/Client';
+import './Dashboard.css';
+import logo from './Twitter.svg';
+import { Header, Container, Image } from 'semantic-ui-react';
 
 class Dashboard extends Component {
-  
   render() {
     let message;
     let pie, image;
-
-    // Google Logo
-    image = <img src={google} width="250" height="100" alt="Google logo"/>;
 
     //Bitcoin chart
     message = <Chart />;
@@ -22,28 +19,37 @@ class Dashboard extends Component {
     //Pie graph
     pie = <Pie />;
     return (
-      <div >
-        <h2>Welcome to Dashboard!!</h2>
-        <table  cellPadding= "50px" width="200px">
+      <div>
+        <Header as="h2">Welcome to Dashboard!</Header>
+        <Image src={Google} size="small" />
+        <table cellPadding="50px" width="200px">
           <tr className="App">
-            <th><img src={logo} alt="Twitter's logo"/></th>
+            <th>
+              <img src={logo} alt="Twitter's logo" />
+            </th>
             <th className="App-logo">{image}</th>
-        </tr>
-        <tr>
-          <td className="App-button">{<button onClick={() => firebase.auth().signOut() }>Sign out</button>}</td>
-        </tr>
+          </tr>
+          <tr>
+            <td className="App-button">
+              {
+                <button onClick={() => firebase.auth().signOut()}>
+                  Sign out
+                </button>
+              }
+            </td>
+          </tr>
         </table>
-
-        <table cellPadding= "50px" width= "1400px" height="500px" >
+        <table cellPadding="50px" width="1400px" height="500px">
           <tr>
             <th>{message}</th>
-            <th><Client /></th> 
-        </tr>
-        <tr>
-          <td>{pie}</td>
-        </tr>
+            <th>
+              <Client />
+            </th>
+          </tr>
+          <tr>
+            <td>{pie}</td>
+          </tr>
         </table>
-
       </div>
     );
   }
