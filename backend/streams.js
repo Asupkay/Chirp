6e79bfe8e529349bf62013acddae1e326ca33c98
Kyle <kyle.rozanitis@gmail.com>
2019-04-04 16:11:44 -0400

    Change timestamp from UTC to epoch and display chart axis in EST

--- backend/streams.js
+++ backend/streams.js
@@ -12,25 +12,24 @@ const client = new Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-
-const initializeStreams = (io) => {
+const initializeStreams = io => {
   //Client to track Google tweets
-  client.stream('statuses/filter', {track: 'Google'}, (stream) => {
+  client.stream('statuses/filter', { track: 'Google' }, stream => {
     let sentimentScore = 0;
     let numTweets = 0;
     let languages = {};
 
     //Every minute actions for collecting out data
     setInterval(async () => {
-      console.log(`Average sentiment = ${sentimentScore/numTweets}`);
+      console.log(`Average sentiment = ${sentimentScore / numTweets}`);
 
       let eventObject = {
-        time: new Date(),
-        averageSentiment: sentimentScore/numTweets
+        time: +new Date(),
+        averageSentiment: sentimentScore / numTweets
       };
 
       io.to('Google').emit('new sentiment', eventObject);
-      io.to('Google').emit('new language nums', languages); 
+      io.to('Google').emit('new language nums', languages);
 
       firebase.writeSentimentData(eventObject, 'Google');
 
@@ -38,35 +37,34 @@ const initializeStreams = (io) => {
         count = await firebase.getLanguageCount(key, 'Google');
         count += languages[key];
         firebase.writeLanguageData(key, count, 'Google');
-      };
-      
+      }
+
       sentimentScore = 0;
       numTweets = 0;
       languages = {};
-    }, 60 * 1000)
+    }, 60 * 1000);
 
     //When a tweet is emitted over the stream
-    stream.on('data', (event) => {
+    stream.on('data', event => {
       let lang = franc(event.text);
 
       if (lang in languages) {
         languages[lang] += 1;
       } else {
         languages[lang] = 1;
-      };
-      
-      if(lang == 'eng') {
+      }
+
+      if (lang == 'eng') {
         let tweetSentimentScore = sentiment.analyze(event.text);
         sentimentScore += tweetSentimentScore.comparative;
         numTweets++;
       }
-
     });
-    
-    stream.on('error', (error) => {
+
+    stream.on('error', error => {
       throw error;
     });
   });
-}
+};
 
 module.exports = initializeStreams;
