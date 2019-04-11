139c56840a2813f5b52b67e04e8f0a682f0f96ce
Kyle <kyle.rozanitis@gmail.com>
2019-04-11 11:38:04 -0400

    Add functionality to query NewsApi and store in firebase

---
+++ backend/news.js
@@ -0,0 +1,30 @@
+const NewsAPI = require('newsapi');
+const firebase = require('./database');
+
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+
+const getNews = company => {
+  setInterval(async () => {
+    newsapi.v2
+      .topHeadlines({
+        q: company,
+        language: 'en'
+      })
+      .then(response => {
+        console.log(response);
+        Object.entries(response.articles).forEach(([key, value]) => {
+          let eventObject = {
+            time: +new Date(),
+            title: value.title,
+            author: value.author,
+            description: value.description,
+            url: value.url
+          };
+          console.log(eventObject);
+          firebase.writeNewsData(eventObject, company);
+        });
+      });
+  }, 60 * 1000);
+};
+
+module.exports = getNews;
