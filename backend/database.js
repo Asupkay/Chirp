fb73c5986767373e445fdc2f72779ad1187d0840
Alex Supkay <asupkay1124@gmail.com>
2019-05-09 20:08:07 -0400

    Tests

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('./serviceAccountKey');
 
 require('dotenv').config();
 
